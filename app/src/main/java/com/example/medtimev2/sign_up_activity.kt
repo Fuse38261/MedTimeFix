8077a69bed76ea8175aecfd56ee18da7fa914353
Fuse PC <techasit2550@gmail.com>
2024-01-16 19:19:39 +0700

    - Login and register UI

---
+++ app/src/main/java/com/example/medtimev2/sign_up_activity.kt
@@ -0,0 +1,63 @@
+/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		sign_up
+	 *	@date 		Tuesday 16th of January 2024 04:36:52 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+package com.example.medtimev2
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.medtimev2.R
+
+class sign_up_activity : Activity() {
+    private var _bg__sign_up_ek2: View? = null
+    private var _bg__page_header_ek1: View? = null
+    private var login: TextView? = null
+    private var sign_up_ek3: TextView? = null
+    private var _bg__input_text_ek1: View? = null
+    private var bg: ImageView? = null
+    private var name: TextView? = null
+    private var _bg__input_text_ek3: View? = null
+    private var bg_ek1: ImageView? = null
+    private var email: TextView? = null
+    private var _bg__input_text_ek5: View? = null
+    private var bg_ek2: ImageView? = null
+    private var password: TextView? = null
+    private var show: TextView? = null
+    private var _bg__button_primary_ek1: View? = null
+    private var sign_up_ek4: TextView? = null
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_up)
+        _bg__sign_up_ek2 = findViewById<View>(R.id._bg__sign_up_ek2) as View
+        _bg__page_header_ek1 = findViewById<View>(R.id._bg__page_header_ek1) as View
+        login = findViewById<View>(R.id.login) as TextView
+        sign_up_ek3 = findViewById<View>(R.id.sign_up_ek3) as TextView
+        _bg__input_text_ek1 = findViewById<View>(R.id._bg__input_text_ek1) as View
+        bg = findViewById<View>(R.id.bg) as ImageView
+        name = findViewById<View>(R.id.name) as TextView
+        _bg__input_text_ek3 = findViewById<View>(R.id._bg__input_text_ek3) as View
+        bg_ek1 = findViewById<View>(R.id.bg_ek1) as ImageView
+        email = findViewById<View>(R.id.email) as TextView
+        _bg__input_text_ek5 = findViewById<View>(R.id._bg__input_text_ek5) as View
+        bg_ek2 = findViewById<View>(R.id.bg_ek2) as ImageView
+        password = findViewById<View>(R.id.password) as TextView
+        show = findViewById<View>(R.id.show) as TextView
+        _bg__button_primary_ek1 = findViewById<View>(R.id._bg__button_primary_ek1) as View
+        sign_up_ek4 = findViewById<View>(R.id.sign_up_ek4) as TextView
+
+
+        //custom code goes here
+    }
+}
