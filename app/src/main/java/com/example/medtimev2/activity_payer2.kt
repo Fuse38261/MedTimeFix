9603543abd25c74accca82278364ba4b0fb5e01d
Fuse PC <techasit2550@gmail.com>
2024-01-21 20:19:09 +0700

    - Login and register work, but can't sync data to cloud - Unfinished DropDown Menu

---
+++ app/src/main/java/com/example/medtimev2/activity_payer2.kt
@@ -0,0 +1,39 @@
+/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		doctor
+	 *	@date 		Sunday 21st of January 2024 07:01:28 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+package com.example.medtimev2
+
+import androidx.fragment.app.Fragment
+
+class activity_payer2 : Fragment() {
+
+    //private var binding: activity_payer2? = null
+
+
+    /*override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+
+
+
+
+
+    }*/
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+    }
+}
--- app/src/main/java/com/example/medtimev2/payer.kt
+++ app/src/main/java/com/example/medtimev2/payer.kt
@@ -2,7 +2,11 @@ package com.example.medtimev2
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.*
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class payer : AppCompatActivity() {
